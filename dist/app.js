60bdd516fb232ada92c68390746554f0b6601abe
SHASHI-17 <hibye31583@gmail.com>
2024-02-08 01:42:08 +0530

    cors fixed

--- dist/app.js
+++ dist/app.js
@@ -5,6 +5,7 @@ import NodeCache from "node-cache";
 import { config } from "dotenv";
 import morgan from 'morgan';
 import Stripe from "stripe";
+import cors from 'cors';
 // importing routes 
 import userRoute from "./routes/user.js";
 import productRoute from "./routes/product.js";
@@ -23,6 +24,7 @@ export const stripe = new Stripe(process.env.STRIPE_KEY || "");
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 app.get('/', (req, res) => {
     res.send("API Working with /api/v1");
 });
--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import NodeCache from "node-cache";
 import { config } from "dotenv";
 import morgan from 'morgan'
 import Stripe from "stripe";
+import cors from 'cors';
 
 // importing routes 
 import userRoute from "./routes/user.js"
@@ -27,7 +28,8 @@ export const stripe = new Stripe(process.env.STRIPE_KEY || "");
 // body parser ,default middleware and server intiation
 const app = express();
 app.use(express.json());
-app.use(morgan("dev"))
+app.use(morgan("dev"));
+app.use(cors());
 app.get('/', (req, res) => {  //  testing route
     res.send("API Working with /api/v1");
 });
